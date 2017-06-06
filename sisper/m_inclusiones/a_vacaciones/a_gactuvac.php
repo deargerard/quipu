271e88d8d6fbe01670c32b9c0870330aea8e6b80
mcotrina <mcotrina>
2017-06-06 16:12:28 -0500

    agregando tarea para actualizar estado

--- sisper/m_inclusiones/a_vacaciones/a_gactuvac.php
+++ sisper/m_inclusiones/a_vacaciones/a_gactuvac.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-include ("../php/conexion_sp.php");
-include ("../php/funciones.php");
+include("/var/www/html/sisper/m_inclusiones/php/conexion_sp.php");
+include("/var/www/html/sisper/m_inclusiones/php/funciones.php");
 	$cev=mysqli_query($cone,"SELECT pv.idProVacaciones FROM provacaciones pv INNER JOIN empleadocargo ec ON pv.idEmpleadoCargo=ec.idEmpleadoCargo where date_format (ec.FechaVac, '%m-%d')=date_format(now(),'%m-%d') AND pv.estado=4");
 	while ($rev=mysqli_fetch_assoc($cev)) {
 		$idpv=$rev['idProVacaciones'];
@@ -11,7 +11,7 @@ include ("../php/funciones.php");
 		}else{
 			$msg=date('d/m/Y H:i:s')." -- Error: No se pudo actualizar el estado de las vacaciones. ".mysqli_error($cone);
 		}
-	$archivo=fopen("../../logs/log_cambio_plan_pend.txt", "a") or die("Problemas al crear");
+	$archivo=fopen("/var/www/html/sisper/logs/log_cambio_plan_pend.txt", "a") or die("Problemas al crear");
 	fwrite($archivo,$msg);
 	fwrite($archivo,"\n");
 	fclose($archivo);
