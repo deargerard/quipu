d53196af72d2ac59b047c6a6c55ec60e42f21892
root <gintor@djmail.mpfn.gob.pe>
2018-04-11 08:32:47 -0500

    Corregir constante noti

--- notificaciones/const.php
+++ notificaciones/const.php
@@ -1,5 +1,5 @@
 <?php
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', realpath(dirname(__FILE__)).DS);
-define('URL', "http://192.168.16.205/notificaciones");
+define('URL', "http://sistemas.djcajamarca.mpfn.gob.pe/notificaciones");
 ?>
