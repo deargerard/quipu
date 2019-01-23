9735491d1b4e99110f3b4238be6e21f9c612848f
root <gintor@djmail.mpfn.gob.pe>
2019-01-23 13:21:18 -0500

    Actualizacion anexo7

--- sisper/m_inclusiones/a_tesoreria/pdf_anexo07.php
+++ sisper/m_inclusiones/a_tesoreria/pdf_anexo07.php
@@ -93,7 +93,7 @@ $cc=mysqli_query($cone, "SELECT cs.*, d.Numero, d.Ano, d.Siglas, e.ApellidoPat, 
     </tr>
     <tr>
       <th>LUGAR DE LA COMISIÓN</th>
-      <td colspan="5" align="center"><?php echo disprodep($cone, $rc['idDistrito']); ?></td>
+      <td colspan="5" align="center"><?php echo $rc['destino']; ?></td>
     </tr>
     <tr>
       <td colspan="6"></td>
