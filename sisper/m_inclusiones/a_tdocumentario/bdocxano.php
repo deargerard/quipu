87ccd622c6ce155bf945df71c87d3a7027e56899
Marco Cotrina <marcocotrina@hotmail.com>
2019-09-16 11:32:00 -0500

    actualizacion consultas

--- sisper/m_inclusiones/a_tdocumentario/bdocxano.php
+++ sisper/m_inclusiones/a_tdocumentario/bdocxano.php
@@ -26,11 +26,11 @@ if(accesocon($cone,$_SESSION['identi'],17)){
                         <td><?php echo fnormal($rd['FechaDoc']); ?></td>                        
                     </tr>
                     <tr>
-                        <th colspan="4"><i class="fa fa-align-left text-aqua"></i> DESCRIPCIÓN</th>
+                        <th colspan="4"><i class="fa fa-info-circle text-aqua"></i> ASUNTO</th>
                         
                     </tr>
                     <tr>
-                        <td colspan="4"><?php echo $rd['Descripcion']; ?></td>
+                        <td colspan="4"><?php echo $rd['asunto']; ?></td>
                         
                     </tr>
 <?php
@@ -66,9 +66,11 @@ if(accesocon($cone,$_SESSION['identi'],17)){
                 
 <?php
             $v1=$rd['idDoc'];
-            $ce=mysqli_query($cone, "SELECT ed.*, modnotificacion FROM tdestadodoc ed LEFT JOIN tdmodnotificacion mn ON ed.idtdmodnotificacion=mn.idtdmodnotificacion WHERE ed.idDoc=$v1 ORDER BY ed.fecha DESC;");
+            $ce=mysqli_query($cone, "SELECT ed.*, modnotificacion, tipo, motivo FROM tdestadodoc ed LEFT JOIN tdmodnotificacion mn ON ed.idtdmodnotificacion=mn.idtdmodnotificacion LEFT JOIN tdproveido p ON ed.idtdproveido=p.idtdproveido WHERE ed.idDoc=$v1 ORDER BY ed.fecha DESC;");            
+
             if(mysqli_num_rows($ce)>0){
-?>
+
+                ?>
                 <span class="text-muted" style="font-size: 11px;"><i class="fa fa-refresh text-orange"></i> Actualizado al <?php echo date('d/m/Y h:i:s A'); ?></span>
                 <table class="table table-bordered table-hover">
                     <thead>
@@ -86,7 +88,7 @@ if(accesocon($cone,$_SESSION['identi'],17)){
                 while($re=mysqli_fetch_assoc($ce)){
                     $n++;
                     $ti="";
-                    //if($re['idtdestado']!=7 || $re['idtdestado']!=8 || $re['idtdestado']!=9){
+
                         $fec=$re['fecha'];
                         $cs=mysqli_query($cone, "SELECT fecha FROM tdestadodoc WHERE idDoc=$v1 AND fecha>'$fec' ORDER BY fecha ASC LIMIT 1;");
                         if($rs=mysqli_fetch_assoc($cs)){
@@ -95,11 +97,19 @@ if(accesocon($cone,$_SESSION['identi'],17)){
                             $ti=date('Y-m-d H:i:s');
                         }
                         mysqli_free_result($cs);
-                    //}
+
 ?>
                         <tr>
                             <td><?php echo $n; ?></td>
-                            <td><?php echo estadoDoc($re['idtdestado']); ?></td>
+                            <td>
+                                <?php echo estadoDoc($re['idtdestado']); ?>
+                                <?php if($re['idtdestado']==3){ ?>
+                                <br><i class="fa fa-commenting text-orange"></i> <b class="text-muted"><?php echo $re['tipo']; ?>:</b> <?php echo $re['motivo']; ?> <br>
+                                <?php } ?>
+                                <?php if($re['idtdestado']==5){ ?>
+                                <br><i class="fa fa-motorcycle text-orange"></i> <b class="text-muted"> <?php echo $re['estnotificacion']==1 ? "Notificado" : ($re['estnotificacion']==2 ? "Devuelto" : ""); ?></b> <?php echo $re['modnotificacion']; ?><br><i class="fa fa-calendar text-gray"></i> <?php echo fnormal($re['fecnotificacion']); ?> <br>
+                                <?php } ?>
+                            </td>
                             <td><?php echo date('d/m/Y h:i:s A', strtotime($re['fecha'])); ?></td>
                             <td class="text-orange"><i class="fa fa-clock-o"></i> <?php echo $ti!="" ? diftiempo($fec, $ti) : ""; ?></td>
                             <td>
@@ -108,7 +118,7 @@ if(accesocon($cone,$_SESSION['identi'],17)){
                                     echo nomempleado($cone, $re['idEmpleado']).'<br><small class="text-aqua">'.nomdependencia($cone, $re['idDependencia']).'</small>';
                                 }else{
                                     if(!is_null($re['idtdmesapartes'])){
-                                        echo nommpartes($cone, $re['idtdmesapartes']).(!is_null($re['responsablemp']) ? '<br><small class="text-aqua">'.nomempleado($cone, $re['responsablemp']).'</small>' : '');
+                                        echo nommpartes($cone, $re['idtdmesapartes']).(!is_null($re['asignador']) ? '<br><small class="text-aqua">'.nomempleado($cone, $re['asignador']).'</small>' : '');
                                     }
                                 }
                                 ?>
@@ -117,10 +127,7 @@ if(accesocon($cone,$_SESSION['identi'],17)){
                         <?php if(!is_null($re['observacion'])){ ?>
                         <tr>
                             <td colspan="5">
-                                <?php if($re['idtdestado']==5){ ?>
-                                <b class="text-muted">NOTIFICADO:</b> <?php echo $re['notificado']==1 ? '<i class="fa fa-check-circle text-green"></i> Sí' : '<i class="fa fa-times-circle text-red"></i> No'; ?> | <b class="text-muted">MODO:</b> <i class="fa fa-motorcycle text-purple"></i> <?php echo $re['modnotificacion']; ?> | <b class="text-muted">FECHA NOTIFICACIÓN:</b> <i class="fa fa-calendar text-fuchsia"></i> <?php echo fnormal($re['fecnotificado']); ?> <br>
-                                <?php } ?>
-                                <b class="text-muted">OBSERVACIÓN:</b> <i class="fa fa-info-circle text-yellow"></i> <?php echo $re['observacion']; ?>
+                                <i class="fa fa-info-circle text-yellow"></i> <b class="text-muted">OBSERVACIÓN:</b> <?php echo $re['observacion']; ?>
                             </td>
                         </tr>
                         <?php } ?>    
@@ -130,6 +137,7 @@ if(accesocon($cone,$_SESSION['identi'],17)){
                     </tbody>
                 </table>
 <?php
+
             }
             mysqli_free_result($ce);
 ?>
