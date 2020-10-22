30f4e9d5a1e816482815e3711b58d7ce39b4521a
Gerardo Intor <upgerard@gmail.com>
2020-10-22 13:46:34 -0500

    actualizacion anexo 1

--- sisper/m_inclusiones/a_tesoreria/pdf_anexo01.php
+++ sisper/m_inclusiones/a_tesoreria/pdf_anexo01.php
@@ -71,49 +71,52 @@ $cc=mysqli_query($cone, "SELECT cs.*, d.Numero, d.Ano, d.Siglas, e.ApellidoPat, 
     <tr>
       <th bgcolor="#EAF1DD">Dependencia</th>
       <td colspan="3" align="center"><?php echo substr(html_entity_decode(dependenciaxiecxfecha($cone, $idec, date('Y-m-d', strtotime($rc['FechaIni'])))),0,60); ?></td>
-      <th colspan="2">D.N.I.</th>
+      <th colspan="2" bgcolor="#EAF1DD">D.N.I.</th>
       <td colspan="3" align="center"><?php echo $rc['NumeroDoc']; ?></td>
     </tr>
     <tr>
-      <th>Cargo</th>
+      <th bgcolor="#EAF1DD">Cargo</th>
       <td colspan="3" align="center"><?php echo cargoiec($cone, idecxidexfecha($cone, $rc['idEmpleado'], date('Y-m-d', strtotime($rc['FechaIni'])))); ?></td>
-      <th colspan="2">Regimen</th>
+      <th colspan="2" bgcolor="#EAF1DD">Régimen</th>
       <td colspan="3" align="center"><?php echo condicionlabxiec($cone, $idec); ?></td>
     </tr>
     <tr>
-      <th>Mnemonico</th>
+      <th bgcolor="#EAF1DD">Mnemonico</th>
       <td align="center"><?php echo $rc['mnemonico']; ?></td>
-      <th colspan="2">N° de Cuenta</th>
+      <th colspan="2" bgcolor="#EAF1DD">N° de Cuenta</th>
       <td colspan="5" align="center"></td>
     </tr>
     <tr>
-      <th>Lugar de la comisión</th>
+      <th bgcolor="#EAF1DD">Lugar de la comisión</th>
       <td style="width: 33%;" align="center"><?php echo $rc['destino']; ?></td>
-      <th colspan="2">Doc. Autoriza</th>
+      <th colspan="2" bgcolor="#EAF1DD">Doc. Autoriza</th>
       <td colspan="5" align="center"><?php echo substr($rc['TipoDoc'],0,3).". ".$rc['Numero']."-".$rc['Ano']."-".$rc['Siglas']; ?></td>
     </tr>
     <tr>
-      <th>Motivo de la comisión</th>
+      <th bgcolor="#EAF1DD">Motivo de la comisión</th>
       <td colspan="9" align="center"></td>
     </tr>
     <tr>
-      <th>Fecha salida</th>
+      <th bgcolor="#EAF1DD">Fecha salida</th>
       <td align="center"><?php echo date('d/m/Y', strtotime($rc['FechaIni'])); ?></td>
-      <th colspan="2">Fecha retorno</th>
+      <th colspan="2" bgcolor="#EAF1DD">Fecha retorno</th>
       <td colspan="3" align="center"><?php echo date('d/m/Y', strtotime($rc['FechaFin'])); ?></td>
-      <th colspan="2" align="center">TOTAL HORAS</th>
+      <th colspan="2" align="center" bgcolor="#EAF1DD">TOTAL HORAS</th>
     </tr>
     <tr>
-      <th>Hora salida</th>
+      <th bgcolor="#EAF1DD">Hora salida</th>
       <td align="center"><?php echo date('H:i', strtotime($rc['FechaIni'])); ?> Horas</td>
-      <th colspan="2">Hora retorno</th>
+      <th colspan="2" bgcolor="#EAF1DD">Hora retorno</th>
       <td colspan="3" align="center"><?php echo date('H:i', strtotime($rc['FechaFin'])); ?> Horas</td>
       <td colspan="2" align="center"><?php echo $ho; ?></td>
     </tr>
     <tr>
-      <th colspan="2" align="center">CONCEPTO</th>
-      <th colspan="6" align="center">DÍAS</th>
-      <th rowspan="2" style="width: 7%;" align="center">TOTAL</th>
+      <td colspan="9" style="border-left: none; border-right: none;"></td>
+    </tr>
+    <tr>
+      <th colspan="2" align="center" bgcolor="#D7E4BC">CONCEPTO</th>
+      <th colspan="6" align="center" bgcolor="#D7E4BC">DÍAS</th>
+      <th rowspan="2" style="width: 7%;" align="center" bgcolor="#D7E4BC">TOTAL</th>
     </tr>
     <tr>
       <th colspan="2">Viáticos</th>
@@ -209,7 +212,7 @@ $cc=mysqli_query($cone, "SELECT cs.*, d.Numero, d.Ano, d.Siglas, e.ApellidoPat, 
     mysqli_free_result($cco);
 ?>
     <tr>
-      <th colspan="2">Sub Total</th>
+      <th colspan="2" bgcolor="#D7E4BC" align="right">SUB TOTAL</th>
       <th align="right"><?php echo n_2decimales($sv1[1]); ?></th>
       <th align="right"><?php echo n_2decimales($sv1[2]); ?></th>
       <th align="right"><?php echo n_2decimales($sv1[3]); ?></th>
@@ -261,7 +264,7 @@ $cc=mysqli_query($cone, "SELECT cs.*, d.Numero, d.Ano, d.Siglas, e.ApellidoPat, 
     }
 ?>
     <tr>
-      <th colspan="2">TOTAL</th>
+      <th colspan="2" bgcolor="#D7E4BC" align="right">TOTAL</th>
       <th align="right"><?php echo n_2decimales($sv1[1]+$sv2[1]); ?></th>
       <th align="right"><?php echo n_2decimales($sv1[2]+$sv2[2]); ?></th>
       <th align="right"><?php echo n_2decimales($sv1[3]+$sv2[3]); ?></th>
@@ -282,7 +285,19 @@ $cc=mysqli_query($cone, "SELECT cs.*, d.Numero, d.Ano, d.Siglas, e.ApellidoPat, 
       <th colspan="7" align="center">Sello de Pagado y Firma del Encargado Único del FPPE</th>
     </tr>
     <tr>
-      <th colspan="9">NOTA:</th>
+      <td colspan="9" style="border-left: none; border-right: none;"></td>
+    </tr>
+    <tr>
+      <th colspan="9">
+        <b>NOTA:</b><br>
+        <p>a) La Rendición de Cuentas deberá ser presentada dentro de los 05 días calendario después de culminada la comisión. Y cuando dicha Comisión sea en el exterior el plazo máximo para presentar la rendición es de 10 días calendario. Art. 68.3 de la Directiva de Tesorería N° 001-2007-EF/77.15, aprobado con Resolución Directoral N° 002-2007-EF/77.15. <b>En caso de incumplimiento autorizo se me establezca responsabilidad económica y se descuente de mis remuneraciones y/o Honorarios Profesionales (CAS) en caso de no justificar el importe recibido y/o ser sancionado conforme al Reglamento Interno de Trabajo</b>.</p>
+        <p>b) Por razones de cierre del Ejercicio Presupuestal del año en curso, la atención de viáticos se efectuará solo hasta el 16 de diciembre.</p>
+        <p>c) De existir saldo, el comisionado debe devolver el dinero dentro de los cinco (05) días calendario contados desde la culminación de la comisión, a la dependencia que le asigno los fondos, recibiendo a cambio el "Recibo de Caja" correspondiente.</p>
+        <p>d) La Declaración Jurada solo será aplicable por concepto de movilidad Local y movilidad de embarque, utilizando para ello el formato de "Declaración Jurada de Viáticos y Asignaciones por Comisión de Servicios en el Territorio Nacional" - Anexo 02.</p>
+        <p>e) Los Gastos de Alojamiento y Alimentación, así como cualquier otra asignación adicional, deberán ser sustentados con documentos reconocidos por SUNAT (Factura, Boletas de Ventas, Tickets electrónicos, etc.). Emitidos a nombre del Ministerio Público, Utilizando el Formato de Rendición documentaria (Anexo 04), en caso la comisión de servicios tenga el carácter de reservado o por motivos de seguridad del comisionado, podrán ser emitidos a nombre del comisionado.</p>
+        <p>f) En caso de existir comisionados que tengan viáticos pendientes de rendición, no se le otorgaran más fondos hasta que rinda lo adeudado.</p>
+        <p>g) El Gasto en Alojamiento es obligatorio, caso contrario procederá a devolver el equivalente a 100 soles por día no sustentado.</p>
+      </th>
     </tr>
   </table>
 <?php
