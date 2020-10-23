e0161331bcba7276898d7e8057f5a4ac10a4e1ee
Gerardo Intor <upgerard@gmail.com>
2020-10-22 22:49:52 -0500

    actualizacion anexo2_4

--- sisper/m_inclusiones/a_tesoreria/te_anexo02pdf.php
+++ sisper/m_inclusiones/a_tesoreria/te_anexo02pdf.php
@@ -23,7 +23,7 @@ use Spipu\Html2Pdf\Html2Pdf;
 
 -->
 </style>
-<page backtop="18mm" backbottom="5mm" backleft="6mm" backright="6mm" style="font-size: 9px;"> 
+<page backtop="18mm" backbottom="5mm" backleft="6mm" backright="6mm" style="font-size: 10px;"> 
     <page_header> 
       <table class="page_header">
           <tr>
