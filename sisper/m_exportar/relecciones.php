e40838bbb3a49546834c7a38e79675054ec0e83a
Gerardo Intor <upgerard@gmail.com>
2021-01-25 00:09:21 -0500

    Modulo elecciones v2

--- sisper/m_exportar/relecciones.php
+++ sisper/m_exportar/relecciones.php
@@ -120,7 +120,7 @@ if(isset($_GET['eleccion']) && !empty($_GET['eleccion'])){
 			<td align="center" style="font-size: 10px;"><?php echo docidentidad($cone, $rv['empleado_id']); ?></td>
 			<td align="center"><?php echo wordwrap(html_entity_decode(nomempleado($cone, $rv['empleado_id'])),30,"<br/>\n",true); ?></td>
 			<td align="center"><?php echo wordwrap(html_entity_decode(cargoe($cone, $rv['empleado_id'])),40,"<br/>\n",true); ?></td>
-			<td><?php echo wordwrap(html_entity_decode(dependenciae($cone, $rv['empleado_id'])),45,"<br/>\n",true); ?></td>
+			<td><?php echo wordwrap(html_entity_decode(dependenciae($cone, $rv['empleado_id'])),50,"<br/>\n",true); ?></td>
 		</tr>
 <?php
 			}
