b714ed8d01ad5c43fbea12cf6e3c534dafc6e78a
Gerardo Intor <upgerard@gmail.com>
2021-02-01 09:13:43 -0500

    Incautaciones new v1

--- incautaciones_new/conect.php
+++ incautaciones_new/conect.php
@@ -1,7 +1,7 @@
 <?php
 function conect() 
 {
-$mysqli = new mysqli("sisweb","Ministeri0#23", "new_custodia_bd");
+$mysqli = new mysqli(localhost, "sisweb","Ministeri0#23", "new_custodia_bd");
 if ($mysqli->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 	exit(); 
